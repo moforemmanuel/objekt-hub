66237e8d45e9a49daf420804bc9b85bd45d7bdcf
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2026-02-09 12:56:18 +0100

    chore(web): configure image remotePatterns

--- packages/web/next.config.ts
+++ packages/web/next.config.ts
@@ -3,6 +3,19 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 's3.eu-central-003.backblazeb2.com',
+        pathname: '/objekthub-assets/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cloudflare-b2.objekt-hub.workers.dev',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
